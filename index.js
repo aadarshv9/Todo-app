2b2b174ec4cf9e0676fa85bdcd95dd3bda860853
Aadarsh Verma <aadarshv9@gmail.com>
2020-05-29 22:41:20 +0530

    Created my express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const port = 8000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server at ${port}`);
+        return;
+    }
+    console.log(`Server is running on ${port}`);
+})
