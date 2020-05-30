f5069b524837e6f53862eb8a9474d798d08c4bb4
Aadarsh Verma <aadarshv9@gmail.com>
2020-05-30 14:35:01 +0530

    Setup home template

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.set('views', './views');
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
+app.get('/', function(req, res){
+    res.render('home');
+    return;
+})
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server at ${port}`);
